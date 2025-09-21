12b5e128811158165882922bbacf2b10b907fbc1
MinaMorgan <mina.m.morgan@outlook.com>
2025-09-21 04:06:47 +0300

    Delete Account test and webDriver wait added
    Delete Account Test added
    Explicit and Implicit Wait added

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -3,11 +3,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import java.time.Duration;
 
 public class login {
     WebDriver driver;
@@ -26,6 +29,7 @@ public class login {
         driver.findElement(By.xpath("//input[@data-qa='login-email']")).sendKeys("emaill@email.com");
         driver.findElement(By.xpath("//input[@data-qa='login-password']")).sendKeys("1234");
         driver.findElement(By.xpath("//button[@data-qa='login-button']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));   // Implicit wait
 
         String actualURL = driver.getCurrentUrl();
         String expectedURL = "https://www.automationexercise.com/";
@@ -34,10 +38,13 @@ public class login {
 
     @Test
     public void signUpTest(){
-
         driver.findElement(By.xpath("//input[@data-qa='signup-name']")).sendKeys("Mina");
         driver.findElement(By.xpath("//input[@data-qa='signup-email']")).sendKeys("emaill@email.com");
         driver.findElement(By.xpath("//button[@data-qa='signup-button']")).click();
+
+        // Explicit wait
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("id_gender1")));
 
         driver.findElement(By.id("id_gender1")).click();
         driver.findElement(By.id("password")).sendKeys("1234");
@@ -67,10 +74,23 @@ public class login {
         driver.findElement(By.id("zipcode")).sendKeys("12345");
         driver.findElement(By.id("mobile_number")).sendKeys("0666666");
         driver.findElement(By.xpath("//button[@data-qa='create-account']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));   // Implicit wait
 
         String actualURL = driver.getCurrentUrl();
         String expectedURL = "https://www.automationexercise.com/account_created";
         Assert.assertEquals(actualURL, expectedURL, "Failed to sign up");
+    }
+
+    @Test
+    public void deleteAccountTest(){
+        signInTest();
+
+        driver.findElement(By.linkText("Delete Account")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));   // Implicit wait
+
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "https://www.automationexercise.com/delete_account";
+        Assert.assertEquals(actualURL, expectedURL, "Failed to delete account");
     }
 
     @AfterTest
